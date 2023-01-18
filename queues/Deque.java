b3fee5ba302ad46031f08ecfeda973e0dbcda74c
chuangcius <chuangcius@gmail.com>
2023-01-18 22:55:03 +0800

    week 2: add Deque

---
+++ queues/Deque.java
@@ -0,0 +1,182 @@
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    private class Node {
+        private final Item item;
+        private Node pre;
+        private Node next;
+
+        public Node(Item item) {
+            this.item = item;
+        }
+    }
+
+    // construct an empty deque
+    public Deque() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return this.size;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+        Node oldHead = this.head;
+        this.head = new Node(item);
+        if (this.size == 0) {
+            this.tail = this.head;
+        } else {
+            this.head.next = oldHead;
+            oldHead.pre = this.head;
+        }
+        this.size++;
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+        Node oldTail = this.head;
+        this.tail = new Node(item);
+        if (this.size == 0) {
+            this.head = this.tail;
+        } else {
+            this.tail.pre = oldTail;
+            oldTail.next = this.tail;
+        }
+        this.size++;
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        if (this.size == 0) {
+            throw new NoSuchElementException();
+        }
+        Item item = this.head.item;
+        this.head = this.head.next;
+        if (this.head != null) {
+            this.head.pre = null;
+        } else {
+            this.tail = null;
+        }
+        this.size--;
+        return item;
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        if (this.size == 0) {
+            throw new NoSuchElementException();
+        }
+        Item item = this.tail.item;
+        this.tail = this.tail.pre;
+        if (this.tail != null) {
+            this.tail.next = null;
+        } else {
+            this.head = null;
+        }
+        this.size--;
+        return item;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return new DqIterator();
+    }
+
+    private class DqIterator implements Iterator<Item> {
+        private Node current = head;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            if (current == null) {
+                throw new java.util.NoSuchElementException();
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException();
+        }
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        Deque<String> deque = new Deque<>();
+        StdOut.println(deque.size());
+        StdOut.println(deque.isEmpty());
+        StdOut.println("=========");
+        try {
+            deque.removeFirst();
+        } catch (Exception e) {
+            StdOut.println(e);
+        }
+        try {
+            deque.removeLast();
+        } catch (Exception e) {
+            StdOut.println(e);
+        }
+        StdOut.println("=========");
+        deque.addFirst("First_1");
+        deque.addFirst("First_2");
+        deque.addLast("Last_1");
+        deque.addLast("Last_2");
+        try {
+            deque.addLast(null);
+        } catch (Exception e) {
+            StdOut.println(e);
+        }
+        try {
+            deque.addFirst(null);
+        } catch (Exception e) {
+            StdOut.println(e);
+        }
+        StdOut.println("=========");
+        StdOut.println(deque.size());
+        StdOut.println(deque.isEmpty());
+        StdOut.println("=========");
+        Iterator<String> iterator = deque.iterator();
+        while (iterator.hasNext()) {
+            StdOut.println(iterator.next());
+        }
+        try {
+            iterator.remove();
+        } catch (Exception e) {
+            StdOut.println(e);
+        }
+        StdOut.println("=========");
+        StdOut.println(deque.removeFirst());
+        StdOut.println(deque.removeLast());
+        StdOut.println(deque.removeLast());
+        StdOut.println("=========");
+        StdOut.println(deque.size());
+        StdOut.println(deque.isEmpty());
+    }
+
+}
