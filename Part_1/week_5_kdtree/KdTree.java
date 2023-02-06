0128c4139e1097f7420c181e6842abc59bda2adf
chuangcius <chuangcius@gmail.com>
2023-02-06 16:27:07 +0800

    week 5: optimize KdTree.java part 4

--- Part_1/week_5_kdtree/KdTree.java
+++ Part_1/week_5_kdtree/KdTree.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,18 +18,14 @@ public class KdTree {
 
     private static class Node {
         final Point2D point;
-        final RectHV rect;
-        Node left = null;
-        Node right = null;
+        RectHV rect;
+        Node left;
+        Node right;
 
         Node(Point2D point, RectHV rect) {
             this.point = point;
             this.rect = rect;
         }
-    }
-
-    private enum Direction {
-        HORIZONTAL, VERTICAL
     }
 
     // is the set empty?
@@ -46,33 +43,29 @@ public class KdTree {
         if (p == null) {
             throw new IllegalArgumentException();
         }
-        this.root = this.insert(this.root, p, 0, 0, 1, 1, Direction.HORIZONTAL);
+        this.root = this.insert(this.root, p, 0, 0, 1, 1, true);
     }
 
-    private Node insert(Node node, Point2D p, double xmin, double ymin, double xmax, double ymax, Direction direction) {
+    private Node insert(Node node, Point2D p, double xmin, double ymin, double xmax, double ymax, boolean direction) {
         if (node == null) {
             this.size++;
-            RectHV rect = new RectHV(xmin, ymin, xmax, ymax);
-            return new Node(p, rect);
+            return new Node(p, new RectHV(xmin, ymin, xmax, ymax));
         }
         if (Objects.equals(p, node.point)) {
             return node;
         }
-        switch (direction) {
-            case HORIZONTAL:
-                if (Point2D.X_ORDER.compare(p, node.point) < 0) {
-                    node.left = this.insert(node.left, p, node.rect.xmin(), node.rect.ymin(), node.point.x(), node.rect.ymax(), Direction.VERTICAL);
-                } else {
-                    node.right = this.insert(node.right, p, node.point.x(), node.rect.ymin(), node.rect.xmax(), node.rect.ymax(), Direction.VERTICAL);
-                }
-                break;
-            case VERTICAL:
-                if (Point2D.Y_ORDER.compare(p, node.point) < 0) {
-                    node.left = this.insert(node.left, p, node.rect.xmin(), node.rect.ymin(), node.rect.xmax(), node.point.y(), Direction.VERTICAL);
-                } else {
-                    node.right = this.insert(node.right, p, node.rect.xmin(), node.point.y(), node.rect.xmax(), node.rect.ymax(), Direction.VERTICAL);
-                }
-                break;
+        if (this.compare(p, node.point, direction) < 0) {
+            if (direction) {
+                node.left = this.insert(node.left, p, node.rect.xmin(), node.rect.ymin(), node.point.x(), node.rect.ymax(), false);
+            } else {
+                node.left = this.insert(node.left, p, node.rect.xmin(), node.rect.ymin(), node.rect.xmax(), node.point.y(), true);
+            }
+        } else {
+            if (direction) {
+                node.right = this.insert(node.right, p, node.point.x(), node.rect.ymin(), node.rect.xmax(), node.rect.ymax(), false);
+            } else {
+                node.right = this.insert(node.right, p, node.rect.xmin(), node.point.y(), node.rect.xmax(), node.rect.ymax(), true);
+            }
         }
         return node;
     }
@@ -82,47 +75,45 @@ public class KdTree {
         if (p == null) {
             throw new IllegalArgumentException();
         }
-        return this.search(this.root, p, Direction.HORIZONTAL);
+        return this.contains(this.root, p, true);
     }
 
-    private boolean search(Node node, Point2D p, Direction direction) {
+    private boolean contains(Node node, Point2D p, boolean direction) {
         if (node == null) {
             return false;
         }
         if (Objects.equals(p, node.point)) {
             return true;
         }
-        int compare = 0;
-        Direction newDirection = direction;
-        switch (direction) {
-            case HORIZONTAL:
-                compare = Point2D.X_ORDER.compare(p, node.point);
-                newDirection = Direction.VERTICAL;
-                break;
-            case VERTICAL:
-                compare = Point2D.Y_ORDER.compare(p, node.point);
-                newDirection = Direction.HORIZONTAL;
-                break;
-        }
-        if (compare < 0) {
-            return this.search(node.left, p, newDirection);
+        if (this.compare(p, node.point, direction) < 0) {
+            return this.contains(node.left, p, !direction);
         } else {
-            return this.search(node.right, p, newDirection);
+            return this.contains(node.right, p, !direction);
         }
     }
 
     // draw all points to standard draw
     public void draw() {
-        this.draw(this.root);
+        this.draw(this.root, true);
     }
 
-    private void draw(Node node) {
+    private void draw(Node node, boolean direction) {
         if (node == null) {
             return;
         }
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
         node.point.draw();
-        this.draw(node.left);
-        this.draw(node.right);
+        StdDraw.setPenRadius();
+        if (direction) {
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line(node.point.x(), node.rect.ymin(), node.point.x(), node.rect.ymax());
+        } else {
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.line(node.rect.xmin(), node.point.y(), node.rect.xmax(), node.point.y());
+        }
+        this.draw(node.left, !direction);
+        this.draw(node.right, !direction);
     }
 
     // all points that are inside the rectangle (or on the boundary)
@@ -157,10 +148,10 @@ public class KdTree {
         if (this.isEmpty()) {
             return null;
         }
-        return this.nearest(this.root, p, this.root.point, Direction.HORIZONTAL);
+        return this.nearest(this.root, p, this.root.point, true);
     }
 
-    private Point2D nearest(Node node, Point2D p, Point2D nearestPoint, Direction direction) {
+    private Point2D nearest(Node node, Point2D p, Point2D nearestPoint, boolean direction) {
         if (node == null) {
             return nearestPoint;
         }
@@ -170,30 +161,26 @@ public class KdTree {
         if (node.point.distanceSquaredTo(p) < nearestPoint.distanceSquaredTo(p)) {
             nearestPoint = node.point;
         }
-        int compare = 0;
-        Direction newDirection = direction;
-        switch (direction) {
-            case HORIZONTAL:
-                compare = Point2D.X_ORDER.compare(p, node.point);
-                newDirection = Direction.VERTICAL;
-                break;
-            case VERTICAL:
-                compare = Point2D.Y_ORDER.compare(p, node.point);
-                newDirection = Direction.HORIZONTAL;
-                break;
-        }
-        if (compare < 0) {
-            nearestPoint = this.nearest(node.left, p, nearestPoint, newDirection);
+        if (this.compare(p, node.point, direction) < 0) {
+            nearestPoint = this.nearest(node.left, p, nearestPoint, !direction);
             if (node.right != null && nearestPoint.distanceSquaredTo(p) > node.right.rect.distanceSquaredTo(p)) {
-                nearestPoint = this.nearest(node.right, p, nearestPoint, newDirection);
+                nearestPoint = this.nearest(node.right, p, nearestPoint, !direction);
             }
         } else {
-            nearestPoint = this.nearest(node.right, p, nearestPoint, newDirection);
+            nearestPoint = this.nearest(node.right, p, nearestPoint, !direction);
             if (node.left != null && nearestPoint.distanceSquaredTo(p) > node.left.rect.distanceSquaredTo(p)) {
-                nearestPoint = this.nearest(node.left, p, nearestPoint, newDirection);
+                nearestPoint = this.nearest(node.left, p, nearestPoint, !direction);
             }
         }
         return nearestPoint;
+    }
+
+    private int compare(Point2D p, Point2D q, boolean even) {
+        if (even) {
+            return Point2D.X_ORDER.compare(p, q);
+        } else {
+            return Point2D.Y_ORDER.compare(p, q);
+        }
     }
 
     // unit testing of the methods (optional)
@@ -224,5 +211,6 @@ public class KdTree {
         System.out.println("=============");
         System.out.println(kdTree.nearest(new Point2D(0.5, 0.5)));
         System.out.println("=============");
+        kdTree.draw();
     }
 }
